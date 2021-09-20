4a97a9a54f2256daef47947fcb375bb58e15861d
Ana Júlia Júlia <85965741+AnaJuliaVancan@users.noreply.github.com>
2021-09-20 18:55:39 +0000

    Notícias adicionadas

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var listagemRouter = require('./routes/listagem');
+var noticia1Router = require('./routes/noticia1');
+var noticia2Router = require('./routes/noticia2');
+var noticia3Router = require('./routes/noticia3');
+var noticia4Router = require('./routes/noticia4');
+var noticia5Router = require('./routes/noticia5');
+var noticia6Router = require('./routes/noticia6');
 
 var app = express();
 
@@ -23,6 +29,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/listagem', listagemRouter);
+app.use('/noticia1', listagemRouter);
+app.use('/noticia2', listagemRouter);
+app.use('/noticia3', listagemRouter);
+app.use('/noticia4', listagemRouter);
+app.use('/noticia5', listagemRouter);
+app.use('/noticia6', listagemRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
